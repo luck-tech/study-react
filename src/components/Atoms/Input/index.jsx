6b768fdaa9c6af42bbfa664f110f873d310b9178
luck <luck@imai.am>
2023-08-17 14:22:27 +0900

    見た目完成

---
+++ src/components/Atoms/Input/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+
+const Input = () => {
+  return (
+    <StyledInput>
+      <input type="text" placeholder="コメントする" />
+      <button class="send">
+        <img src="../../../assets/ei-send.png" />
+      </button>
+    </StyledInput>
+  );
+};
+
+const StyledInput = styled.div`
+  display: flex;
+  > input {
+    width: 100%;
+    font-size: 12px;
+    border-radius: 4px 0 0 4px;
+    border: 1px solid #c4c9d0;
+    padding: 4px 0 4px 8px;
+    &:focus {
+      outline: none;
+    }
+  }
+  .send {
+    background-color: #6f3f1a;
+    border-radius: 0 4px 4px 0;
+    border: none;
+    > img {
+      width: 30px;
+      height: 30px;
+    }
+  }
+`;
+
+export default Input;
