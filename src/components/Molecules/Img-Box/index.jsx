3d5c694e5b7ebef95c100ae4ac3387c237624a2b
luck <luck@imai.am>
2023-08-17 23:13:32 +0900

    一旦完成

---
+++ src/components/Molecules/Img-Box/index.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const ImgBox = () => {
+  const [data, setData] = useState([]);
+  const targetItemId = "1"; // 表示したいアイテムのid
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://2f338ce1-8358-4109-a88b-929d6bbbcd07.mock.pstmn.io/"
+      );
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error("データの取得に失敗しました。", error);
+    }
+  };
+
+  const targetItem = data.find((item) => item.id === targetItemId);
+
+  return (
+    <div>
+      {targetItem && (
+        <div>
+          <StyledMainImg>
+            <img src={targetItem.photo[0].photo} />
+          </StyledMainImg>
+          <StyledSubImg>
+            <img src={targetItem.photo[1].photo} />
+            <img src={targetItem.photo[2].photo} />
+            <img src={targetItem.photo[3].photo} />
+          </StyledSubImg>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const StyledMainImg = styled.div`
+  > img {
+    width: 100%;
+    padding-bottom: 10px;
+  }
+`;
+const StyledSubImg = styled.div`
+  display: flex;
+  > img {
+    padding: 0 5px;
+    width: 33.33%;
+  }
+`;
+
+export default ImgBox;
