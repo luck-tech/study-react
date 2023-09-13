0c67a1e4706424befaa0c436572ae4f910ff1787
luck <luck@imai.am>
2023-09-13 14:58:16 +0900

    購入状態確認進捗

--- src/components/Atoms/Purchase/index.jsx
+++ src/components/Atoms/Purchase/index.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 const Purchase = () => {
+  const [buyer, setBuyer] = useState();
+  const targetItemId = "ef408730-0bef-45f9-9c82-61f602d14f87"; // 表示したいアイテムのid
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://uni-bo.net/api/items/");
+      const jsonData = await response.json();
+      const targetItemData = jsonData.find((item) => item.id === targetItemId);
+      const buyerData = targetItemData.buyer;
+      setBuyer(buyerData); //useStateのセット関数でbuyerにセット
+      console.log(buyerData); //nullになる
+      console.log(buyer); //undefined
+    } catch (error) {
+      console.error("データの取得に失敗しました。", error);
+    }
+  };
+
   return (
     <StyledPurchase>
-      <button className="purchase_button">購入する</button>
+      <button
+        className="purchase_button"
+        style={{
+          backgroundColor: buyer === "null" ? "#6f3f1a" : "#d2b48c",
+        }}
+      >
+        購入する
+      </button>
     </StyledPurchase>
   );
 };
@@ -12,7 +40,6 @@ const Purchase = () => {
 const StyledPurchase = styled.div`
   .purchase_button {
     border: none;
-    background-color: #6f3f1a;
     color: #f5f5f5;
     border-radius: 4px;
     padding: 8px 20px;
